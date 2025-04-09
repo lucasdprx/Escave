79c2f428f3f0648df5be0cdc651ebc10bc677210
PandorTaxe <beaumont.nathan0112@gmail.com>
2025-04-09 10:31:34 +0200

    Add pause menu

---
+++ Escave/Assets/Scripts/Ui/PauseMenu/PauseMenuManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _pauseMenuPanel;
+    [SerializeField] private GameObject _settingsMenuPanel;
+    private bool _isGamePaused = false;
+
+    public void ShowPausePanel(InputAction.CallbackContext context)
+    {
+        PauseGame();
+    }
+
+    public void ResumeGame()
+    {
+        PauseGame();
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ShowSettings()
+    {
+        _settingsMenuPanel.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    private void PauseGame()
+    {
+        _pauseMenuPanel.SetActive(!_pauseMenuPanel.activeSelf);
+        _isGamePaused = !_isGamePaused;
+        if (_isGamePaused)
+        {
+            Time.timeScale = 0f;
+        }
+        else if (!_isGamePaused)
+        {
+            Time.timeScale = 1f;
+        }
+    }
+}
