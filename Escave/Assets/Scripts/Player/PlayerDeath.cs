b1f53642879732e67bcc01104f556068706d36c5
Kureo524 <kokoshibo524@gmail.com>
2025-04-09 15:46:18 +0200

    Save : Timer and Death and Position

--- Escave/Assets/Scripts/Player/PlayerDeath.cs
+++ Escave/Assets/Scripts/Player/PlayerDeath.cs
@@ -4,7 +4,7 @@ using Unity.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class PlayerDeath : MonoBehaviour
+public class PlayerDeath : MonoBehaviour, IDataPersistence
 {
     [Header("Checkpoints")]
     [SerializeField] private List<GameObject> checkpoints; // checkpoint list
@@ -20,17 +20,22 @@ public class PlayerDeath : MonoBehaviour
     public UnityEvent<int> OnDeath;
     public bool _isRestarting = false;
     
-    private void Start()
+    public void LoadData(GameData _gameData)
     {
-        // initialisation of the current checkpoint to the first one in the list
-        if (checkpoints != null && checkpoints.Count > 0)
+        if (_gameData.playerPos != Vector2.zero)
         {
+            transform.position = _gameData.playerPos;
             currentCheckpoint = checkpoints[0];
         }
-        else
-        {
-            Debug.LogError("Aucun checkpoint n'est assign� !");
-        }
+        
+        deathCounter = _gameData.deathCount;
+        OnDeath.Invoke(deathCounter);
+    }
+
+    public void SaveData(ref GameData _gameData)
+    {
+        _gameData.deathCount = deathCounter;
+        _gameData.playerPos = currentCheckpoint.transform.position;
     }
 
     public void PlayerDie()
