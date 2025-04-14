a1b75a41a51eccd208fb1e79a6ce1d741fd137d5
PandorTaxe <beaumont.nathan0112@gmail.com>
2025-04-14 15:59:30 +0200

    PickUpItem: add pick up item ui

--- Escave/Assets/Scripts/items/PioletPickUp.cs
+++ Escave/Assets/Scripts/items/PioletPickUp.cs
@@ -1,19 +1,21 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PioletPickUp : MonoBehaviour
 {
     [SerializeField] private PlayerWallJump _playerWallJump;
-    [SerializeField] private GameObject _pickUpUiShow;
     [SerializeField] private UIItemShow _itemShow;
+    private PlayerInput _playerAction;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            _playerAction = other.GetComponent<PlayerInput>();
             EnablingPiolet();
             DataPersistenceManager.instance.gameData.isPioletEnabled = true;
-            _itemShow.ShowPiolet();
+            _itemShow.ShowPiolet(_playerAction);
         }
     }
     
