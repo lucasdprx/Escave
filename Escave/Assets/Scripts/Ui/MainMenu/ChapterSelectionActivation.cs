44101b1be88efe99caa315f61ee264495b88b924
lucasdprx <150352145+lucasdprx@users.noreply.github.com>
2025-04-17 13:06:20 +0200

    Clean prefab

--- Escave/Assets/Scripts/Ui/MainMenu/ChapterSelectionActivation.cs
+++ Escave/Assets/Scripts/Ui/MainMenu/ChapterSelectionActivation.cs
@@ -5,6 +5,8 @@ public class ChapterSelectionActivation : MonoBehaviour, IDataPersistence
 {
     public void LoadData(GameData _gameData)
     {
+        if (_gameData.chaptersFinished.Count <= 0) return;
+        
         if (_gameData.chaptersFinished[0] == false)
         {
             GetComponent<Button>().interactable = false;
