e084e39cb6d6d10aa7ee13ee13dd9645c6307060
Kureo524 <kokoshibo524@gmail.com>
2025-04-17 13:35:30 +0200

    1ligne changmeent

--- Escave/Assets/Scripts/Ui/MainMenu/ChapterSelectionActivation.cs
+++ Escave/Assets/Scripts/Ui/MainMenu/ChapterSelectionActivation.cs
@@ -9,7 +9,7 @@ public class ChapterSelectionActivation : MonoBehaviour, IDataPersistence
     {
         chapterSelectionButton.interactable = false;
         
-        if (_gameData.chaptersFinished[1] == true)
+        if (_gameData.chaptersFinished.Count > 0 && _gameData.chaptersFinished[1] == true)
             chapterSelectionButton.interactable = true;
     }
 
