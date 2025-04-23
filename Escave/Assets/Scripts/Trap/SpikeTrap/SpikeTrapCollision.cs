b1267327c00967c596eed79ac8e7e096dc35315a
NicolasTre <N.TREZEGUET@lahorde.school>
2025-04-23 15:42:19 +0200

    upgrade trap

--- Escave/Assets/Scripts/Trap/SpikeTrap/SpikeTrapCollision.cs
+++ Escave/Assets/Scripts/Trap/SpikeTrap/SpikeTrapCollision.cs
@@ -5,8 +5,15 @@ public class TrapCollision : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (!collision.gameObject.CompareTag("Player")) return;
-        
-        collision.gameObject.GetComponent<PlayerDeath>().PlayerDie();
-    }
 
+        if (collision.gameObject.TryGetComponent<PlayerDeath>(out var playerDeath))
+        {
+            // Appelle la méthode pour tuer le joueur
+            playerDeath.PlayerDie();
+        }
+        else
+        {
+            Debug.LogWarning("Le joueur n'a pas de composant PlayerDeath attaché !");
+        }
+    }
 }
